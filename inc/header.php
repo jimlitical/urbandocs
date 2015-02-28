3c175dd67d0fe3013869b5640a78dd72552df148
Jimmy Hong <hongj5@spu.edu>
2015-02-28 12:28:45 -0800

    Urban 2

--- inc/header.php
+++ inc/header.php
@@ -19,8 +19,8 @@ header("Pragma: no-cache");   // HTTP/1.0
 <body>
 
 <div id="logo">
-	<a href="default.php" id="jLogo">Urban</a>
-	<a href="default.php" id="quoteLogo">&nbsp;&nbsp;&nbsp;Docs</a>
+	<a href="index.php" id="jLogo">Urban</a>
+	<a href="index.php" id="quoteLogo">&nbsp;&nbsp;&nbsp;Docs</a>
 </div>
 <div id="header">
 <table id="searchtable">
@@ -29,7 +29,7 @@ header("Pragma: no-cache");   // HTTP/1.0
 		<form id="floatleft" action="quote.php" method="get" name="nav">
 			<input id="search" type="text" value="<?php if(!empty($_GET['search'])){echo $_GET['search'];} ?>" placeholder="Search..." name="search"></input>
 			<input type="submit" style="position: absolute; left: -9999px"></input>
-			<input id="homebtn" type="submit" value="Search" onclick="nav.action='home.php'"></input>
+			<input id="lookbtn" type="submit" value="Search" onclick="nav.action='search.php'"></input>
 		</form>
 	<td>
 </tr>
---
+++ search.php
@@ -0,0 +1,9 @@
+<?php require "inc/header.php"?>
+<?php require "inc/intro.php"?>
+
+</body>
+</html>
+
+<?php	// This is the LAST section in a PHP webpage file
+ob_end_flush();
+?>
